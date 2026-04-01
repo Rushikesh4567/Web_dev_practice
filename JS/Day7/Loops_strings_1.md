7e0a5c2c1294bd81b1d8250001fd857c6eb7ac73
Rushikesh4567 <rushikeksh4567@gmail.com>
2026-04-01 07:41:13 +0530

    solved easy level questions of js loops and strings

---
+++ JS/Day7/Loops_strings_1.md
@@ -0,0 +1,7 @@
+🟢 EASY LEVEL
+1.Print numbers from 1 to 10 using for loop
+2.Print even numbers from 0 to 100
+3.Print all characters of a string using for...of
+4.Find length of a string
+5.Convert a string to uppercase
+
---
+++ JS/Day7/script.js
@@ -0,0 +1,31 @@
+//1.
+for(let i = 0; i<10 ;i++)
+{
+    console.log(i);
+}
+
+//2.
+for (let i = 0; i <= 100; i++) 
+{
+    if (i % 2 === 0) 
+    {
+        console.log(i);
+    }
+}
+//3.
+let str1 = "hello";
+
+for (let char of str1) {
+    console.log(char);
+}
+
+//4.
+console.log("Length of string: ",str.length);
+
+//5.
+let str = "Rushikesh";
+console.log(str.toUpperCase());
+
+
+
+
